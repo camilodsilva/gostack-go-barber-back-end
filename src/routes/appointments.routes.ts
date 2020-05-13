570cb9586785d59b7fe6055a3663dcb268f5cc3a
Camilo Silva <camilodsmp@gmail.com>
2020-05-13 19:06:30 -0300

    Create the apointment routes, model and repository

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,19 +1,25 @@
 import { Router } from 'express';
-import { uuid } from 'uuidv4';
+import { startOfHour, parseISO, isEqual } from 'date-fns';
+
+import Appointments from '../modles/Appointments';
+import AppointmentsRepository from '../repositories/AppointmentsRepository';
 
 const routes = Router();
-
-const appointments = [];
+const appointmentsRepository = new AppointmentsRepository();
 
 routes.post('/', (request, response) => {
   const { provider, date } = request.body;
 
-  const appointment = {
-    id: uuid(),
-    provider,
-    date,
-  };
-  appointments.push(appointment);
+  const parsedDate = startOfHour(parseISO(date));
+  const findAppointmentInSameDate = appointments.find(appointment =>
+    isEqual(parsedDate, appointment.date),
+  );
+
+  if (findAppointmentInSameDate) {
+    return response
+      .status(400)
+      .json({ message: 'This appointment is already booked' });
+  }
 
   return response.json(appointment);
 });
